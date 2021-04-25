f4e8b02ed7123489571b84d1062b66c3c8037e19
Nigesh Shakya <nizeshshakya@gmail.com>
2021-04-25 05:39:58 +0100

    Added a scheduled cleaner that cleans up the GPIO states

--- scheduled_cleaner.sh
+++ scheduled_cleaner.sh
@@ -9,34 +9,34 @@ cd $ROOT_PATH
 
 LAUNCHER_PID=`ps aux | egrep "[l]auncher.sh" | awk '{print $2}'`
 PROGRAM_PID=`ps aux | egrep "[s]tart.py" | awk '{print $2}'`
+NOW=$(date +"%m-%d-%Y %H:%M")
 
 touch $STATUS_LOG_PATH
+echo "" > $STATUS_LOG_PATH
 
 if [ -z "$LAUNCHER_PID" ]
 then
-	echo "launcher process is empty" >> $STATUS_LOG_PATH
+	echo "$NOW	launcher process is empty" >> $STATUS_LOG_PATH
 	if [ -z "$PROGRAM_PID" ]
 	then
-		echo "program is also empty. Nothing to do!" >> $STATUS_LOG_PATH
+		echo "$NOW	program is also empty. Nothing to do!" >> $STATUS_LOG_PATH
 	else
-		echo "The programi with PID: $PROGRAM_PID is running without launcher. Exiting program now." >> $STATUS_LOG_PATH
+		echo "$NOW	The program with PID: $PROGRAM_PID is running without launcher. Exiting program now." >> $STATUS_LOG_PATH
+	fi
+
+	kill -9 $PROGRAM_PID >/dev/null
+	python3 ./cleanup.py > /dev/null
+
+else
+	echo "$NOW	launcher process is running with process ID: $LAUNCHER_PID"  >> $STATUS_LOG_PATH
+	if [ -z "$PROGRAM_PID" ]
+	then
+		echo "$NOW	Oops! program with PID: $PROGRAM_PID is not running while launcher is still running. Something is off!" >> $STATUS_LOG_PATH
 		kill -9 $PROGRAM_PID >/dev/null
 		python3 ./cleanup.py > /dev/null
-	fi
-else
-	echo "launcher process is not empty with process ID: $LAUNCHER_PID"  >> $STATUS_LOG_PATH
-	if [ -z "$PROGRAM_PID" ]
-	then
-		echo "Oops! program is not running while launcher is still running. Something is off!" >> $STATUS_LOG_PATH
+
 	else
-		echo "Looks like both launcher and program are running." >> $STATUS_LOG_PATH
+		echo "$NOW	Looks like both launcher and program ($PROGRAM_PID) are running." >> $STATUS_LOG_PATH
 	fi
 fi
-
-#if [ -z "$PROGRAM_PID" ]
-#then
-#	echo "program process is empty" >> $STATUS_LOG_PATH
-#else
-#	echo "program process is not empty with process ID: $PROGRAM_PID" >> $STATUS_LOG_PATH
-#fi
 
