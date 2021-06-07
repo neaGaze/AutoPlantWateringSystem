c8815041e3b491c8e5fc93f0dcd2963f98b5bb0d
Nigesh Shakya <nigeshshakya@Nigeshs-MacBook-Pro.local>
2021-06-07 14:47:20 -0500

    Added a uniq id generator and update sql

--- mysql/activity.py
+++ mysql/activity.py
@@ -1,6 +1,7 @@
 from mysql_connector import MysqlDriver
 from time import time
 from datetime import datetime
+import uuid
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 WATER_START_TYPE = "WATER START"
@@ -10,6 +11,7 @@ class Activity:
 
     # ACTIVITY
     INSERT_ACTIVITY = "INSERT INTO Activity (transaction_id, created_at, channel_id, activity_type, is_open) VALUES (%d, '%s', %d, '%s', %i)"
+    UPDATE_ACTIVITY = "UPDATE Activity SET activity_type='%s' AND is_open=%i WHERE channel_id=%d and transaction_id=%d"
     READ_IS_OPEN = "SELECT channel_id, transaction_id from Activity where is_open=%i"
 
     def __init__(self):
@@ -17,7 +19,7 @@ class Activity:
 
     def insert_water_start_txn(self, channel_id):
         if self._driver:
-            uniq_id = time()
+            uniq_id = uuid.uuid1().int # time()
             cur_time = datetime.now()
             print("QUERY into: "+Activity.INSERT_ACTIVITY % (uniq_id, cur_time.strftime(DATETIME_FORMAT), channel_id, WATER_START_TYPE, True))
             self._driver.insert(lambda cursor: cursor.execute(Activity.INSERT_ACTIVITY % (uniq_id, cur_time.strftime(DATETIME_FORMAT), channel_id, WATER_START_TYPE, True)))
@@ -25,7 +27,8 @@ class Activity:
     def insert_water_end_txn(self, channel_id, txn_id):
         if self._driver:
             cur_time = datetime.now()
-            self._driver.insert(lambda cursor: cursor.execute(Activity.INSERT_ACTIVITY % (txn_id, cur_time.strftime(DATETIME_FORMAT), channel_id, WATER_END_TYPE, False)))
+            #self._driver.insert(lambda cursor: cursor.execute(Activity.INSERT_ACTIVITY % (txn_id, cur_time.strftime(DATETIME_FORMAT), channel_id, WATER_END_TYPE, False)))
+            self._driver.insert(lambda cursor: cursor.execute(Activity.UPDATE_ACTIVITY % (WATER_END_TYPE, False, channel_id, txn_id)))
 
     def read_are_open(self):
         if self._driver:
